b477af2c4883831d49be2aabe6ed65e1453d73d3
ziadeleraky <ziadeleraky1@gmail.com>
2024-09-09 14:21:10 +0300

    fmt.Scan & pointer

--- investment/investment_calculator.go
+++ investment/investment_calculator.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	expectedReturnRate := 5.5
-	var years float64 = 10
+	var investmentAmount, expectedReturnRate, years float64
 	inflationRate := 2.5
+
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
 
 	futureInvestmentValue := investmentAmount * math.Pow((1 + expectedReturnRate / 100), years)
 	futureRealInvestmentValue := futureInvestmentValue / math.Pow((1 + inflationRate / 100), years)
