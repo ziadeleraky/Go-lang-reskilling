95cb9734872c3550b9ea4b83ad0eadf64a767aaa
ziadeleraky <ziadeleraky1@gmail.com>
2024-09-28 15:22:30 +0300

    problem solving: team

---
+++ problem_solving/team/team.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	var count int
+
+	for i := 0; i < n; i++ {
+		var a, b, c int
+		fmt.Scan(&a, &b, &c) // Directly scan the inputs without using a reader
+		if a+b+c >= 2 {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
